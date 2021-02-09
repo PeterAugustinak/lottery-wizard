8a0372a4796f54eb31daac3b6f773b568f4c646d
PeterAugustinak <48356585+PeterAugustinak@users.noreply.github.com>
2021-02-09 14:54:22 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Lottery
+ Lottery app tries to estimate how many attempts you need to win the lottery.
---
+++ src/lottery.py
@@ -0,0 +1,34 @@
+# coding=utf-8
+"""Main class for Lottery project"""
+
+
+class Lottery:
+    """Lottery class"""
+
+    # if user does not want to specify own numbers, every toss the random number will be set
+    specified_numbers = False
+    # after every toss, it will be evaluated how many numbers user guessed correctly
+    guessed_one = 0
+    guessed_two = 0
+    guessed_three = 0
+    guessed_four = 0
+    guessed_five = 0
+    guessed_six = 0
+
+    def __init__(self):
+        pass
+
+    def numbers_input(self):
+        pass
+
+    def parse_and_validate_input(self):
+        pass
+
+    def start_lottery(self):
+        pass
+
+    def check_attempts(self):
+        pass
+
+
+
