1d4e0d6561543faabc9a4c1bc84ff3fef8df18e0
PeterAugustinak <48356585+PeterAugustinak@users.noreply.github.com>
2021-02-09 16:17:42 +0100

    first solution for input and validate numbers

--- src/lottery.py
+++ src/lottery.py
@@ -19,10 +19,35 @@ class Lottery:
         pass
 
     def numbers_input(self):
-        pass
+        """This clarifies if user want to use his own numbers or just leave it for random AI choices"""
+        user_input = input("Please input your 6 numbers from 1 to 49)\n"
+                           "Example: 2 7 13 28 29 32 46\n"
+                           "If you want every toss random numbers, just press ENTER.")
 
-    def parse_and_validate_input(self):
-        pass
+        validation = self.parse_and_validate_input(user_input) if user_input != "" else "aa"
+        if type(validation) == list:
+            print(f"Your numbers for lottery are: {[' '.join(i) for i in validation]}")
+            self.specified_numbers = validation
+        else:
+            print(validation)
+            self.numbers_input()
+
+    @staticmethod
+    def parse_and_validate_input(user_input):
+        """This will parse and validate user input. If the input is incorrect, back to initial question"""
+        potential_numbers = [element for element in user_input.split()]
+        correct_numbers = []
+        for element in potential_numbers:
+            try:
+                correct_numbers.append(int(element))
+            except ValueError:
+                return "Please input numbers the way described."
+        corrected_numbers = [number for number in correct_numbers if number in range(1, 50)]
+        if len(corrected_numbers) == 6:
+            return corrected_numbers
+        else:
+            return "Please input exactly 6 numbers from 1 to 49."
+
 
     def start_lottery(self):
         pass
@@ -31,4 +56,5 @@ class Lottery:
         pass
 
 
-
+l = Lottery()
+l.numbers_input()
