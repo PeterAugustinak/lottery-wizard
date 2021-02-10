494972dbe3ca5537abf3232b4a9b6af85ed67c05
PeterAugustinak <48356585+PeterAugustinak@users.noreply.github.com>
2021-02-10 10:20:36 +0100

    Complete first draft solution
    need to resolve recursion problem

--- src/lottery.py
+++ src/lottery.py
@@ -4,13 +4,20 @@
 # standard library imports
 import random
 from os import system
+# external library imports
+from prettytable import PrettyTable
+
 
 class Lottery:
     """Lottery class"""
 
     # if user does not want to specify own numbers, every toss the random number will be set
     specified_numbers = False
+    # current numbers
+    lottery_numbers = None
+    drawn_numbers = None
     # after every toss, it will be evaluated how many numbers user guessed correctly
+    guessed_zero = 0
     guessed_one = 0
     guessed_two = 0
     guessed_three = 0
@@ -25,6 +32,7 @@ class Lottery:
 
     def numbers_input(self):
         """This clarifies if user want to use his own numbers or just leave it for random AI choices"""
+        system('cls')
         user_input = input("Please input your 6 numbers from 1 to 49)\n"
                            "Example: 2 7 13 28 29 32 46\n"
                            "If you want every toss random numbers, just press ENTER.")
@@ -47,33 +55,72 @@ class Lottery:
 
     def decide_lottery_numbers(self):
         """This check before lottery start if user speified numbers, if not, it picks random before every toss"""
-        lottery_numbers = sorted(self.specified_numbers if self.specified_numbers else self.random_numbers())
-        system('cls')
+        self.lottery_numbers = sorted(self.specified_numbers if self.specified_numbers else self.random_numbers())
 
-        self.start_lottery(lottery_numbers)
+        self.start_lottery()
 
     @staticmethod
     def random_numbers():
         """Picks 6 random numbers instead of the user"""
         return [random.randrange(1, 50) for _ in range(6)]
 
-    def start_lottery(self, lottery_numbers):
+    def start_lottery(self):
         """This will toss lottery"""
+        system('cls')
         print(f"Toss #{self.toss_counter}")
-        tossed_numbers = sorted(self.random_numbers())
+        self.drawn_numbers = sorted(self.random_numbers())
 
-        print(f"Your lottery numbers are: {lottery_numbers}")
-        print(f"Tossed numbers are: {tossed_numbers}")
+        print(f"Your lottery numbers are: {self.lottery_numbers}")
+        print(f"Drawn numbers are: {self.drawn_numbers}")
 
-        self.evaluate_round(lottery_numbers, tossed_numbers)
-        self.toss_counter += 1
+        self.evaluate_round()
+
+        if self.guessed_six != 0:
+            self.lottery_won()
+
+        # if self.toss_counter % 1000 == 0:
+        self.print_results()
         self.decide_lottery_numbers()
 
-    def evaluate_round(self, lottery_numbers, tossed_nubers):
-        """This will compare user numbers against tossed numbers for the round"""
-        pass
+    def evaluate_round(self):
+        """This will compare user numbers against drawn numbers for the round"""
+        guessed = len(set(self.lottery_numbers).intersection(self.drawn_numbers))
+
+        if guessed == 0: self.guessed_zero += 1
+        if guessed == 1: self.guessed_one += 1
+        if guessed == 2: self.guessed_two += 1
+        if guessed == 3: self.guessed_three += 1
+        if guessed == 4: self.guessed_four += 1
+        if guessed == 5: self.guessed_five += 1
+        if guessed == 6: self.guessed_six += 1
+        self.toss_counter += 1
+
+    def lottery_won(self):
+        """In case of 6 numbers of 6 was guessed correctly - means lottery is won, the tossing will stop"""
+        system('cls')
+        print(f"!!! YOU WON ON TOSS #{self.toss_counter}!!!")
+        print(f"Your numbers was {self.lottery_numbers} and drawn numbers was {self.drawn_numbers}!!!")
+        print(f'If you will put a lottery 1 per week, you need approximatelly {self.count_years()} to win.')
+
+    def print_results(self):
+        """This prints current result after every 1 000 toss"""
+        table = PrettyTable()
+
+        table.field_names = ["Guessed", "Times"]
+        table.add_row(["0 numbers", self.guessed_zero])
+        table.add_row(["1 number", self.guessed_one])
+        table.add_row(["2 numbers", self.guessed_two])
+        table.add_row(["3 numbers", self.guessed_three])
+        table.add_row(["4 numbers", self.guessed_four])
+        table.add_row(["5 numbers", self.guessed_five])
+        table.add_row(["6 numbers", self.guessed_six])
+
+        print(table)
+
+    def count_years(self):
+        """This counts the time evaluated to win"""
+        return round(self.toss_counter / 52, 0)
 
 
+Lottery().numbers_input()
 
-l = Lottery()
-l.numbers_input()
