0e6d2172b0fad4a2b06066efe7d512776bf0d04c
PeterAugustinak <48356585+PeterAugustinak@users.noreply.github.com>
2021-02-11 11:12:19 +0100

    solution for both user defined + random numbers first draft

--- src/lottery.py
+++ src/lottery.py
@@ -14,19 +14,18 @@ from prettytable import PrettyTable
 class Lottery:
     """Lottery class"""
     lang = None
-    # if user does not want to specify own numbers, every toss the random number will be set
-    specified_numbers = False
-    # current numbers
-    lottery_numbers = None
+    # numbers for lottery
+    defined_numbers_for_draw = None
+    random_numbers_for_draw = None
     drawn_numbers = None
     # after every toss, it will be evaluated how many numbers user guessed correctly
-    guessed_zero = 0
-    guessed_one = 0
-    guessed_two = 0
-    guessed_three = 0
-    guessed_four = 0
-    guessed_five = 0
-    guessed_six = 0
+    guessed_zero = [0, 0]
+    guessed_one = [0, 0]
+    guessed_two = [0, 0]
+    guessed_three = [0, 0]
+    guessed_four = [0, 0]
+    guessed_five = [0, 0]
+    guessed_six = [0, 0]
     # counter of lottery tosses
     toss_counter = 1
 
@@ -41,20 +40,19 @@ class Lottery:
               "2. SVK\n")
 
         lang_input = msvcrt.getch()
-        if int(lang_input) == 2:
-            self.lang = 1
-        else:
+        if int(lang_input) != 2:
             self.lang = 0
+        else:
+            self.lang = 1
 
+        system('cls')
         self.numbers_input()
 
     def numbers_input(self):
         """This clarifies if user want to use his own numbers or just leave it for random AI choices"""
-        system('cls')
-        user_input = input(f"{texts['intro1'][self.lang]}\n{texts['intro2'][self.lang]}\n{texts['intro3'][self.lang]}")
+        user_input = input(f"{texts['intro1'][self.lang]}\n{texts['intro2'][self.lang]}\n-> ")
 
-        self.specified_numbers = self.parse_and_validate_input(user_input) if user_input != "" else False
-        self.decide_lottery_numbers()
+        self.defined_numbers_for_draw = self.parse_and_validate_input(user_input)
         self.start_lottery()
 
     def parse_and_validate_input(self, user_input):
@@ -74,59 +72,96 @@ class Lottery:
     def start_lottery(self):
         """This will toss lottery"""
         system('cls')
-        while self.guessed_six == 0:
-            self.decide_lottery_numbers()
+        while self.guessed_six[0] == 0 and self.guessed_six[1] == 0:
+            self.random_numbers_for_draw = sorted(self.random_numbers())
             self.drawn_numbers = sorted(self.random_numbers())
-            self.evaluate_round()
+            self.evaluate_round(self.defined_numbers_for_draw, 0)
+            self.evaluate_round(self.random_numbers_for_draw, 1)
 
-            # if self.toss_counter % 1000 == 0:
             self.print_results()
+            self.toss_counter += 1
 
         self.lottery_won()
-
-    def decide_lottery_numbers(self):
-        """This check before lottery start if user speified numbers, if not, it picks random before every draw"""
-        self.lottery_numbers = sorted(self.specified_numbers if self.specified_numbers else self.random_numbers())
 
     @staticmethod
     def random_numbers():
         """Picks 6 random numbers instead of the user"""
         return [random.randrange(1, 50) for _ in range(6)]
 
-    def evaluate_round(self):
-        """This will compare user numbers against drawn numbers for the dra"""
-        guessed = len(set(self.lottery_numbers).intersection(self.drawn_numbers))
+    def evaluate_round(self, numbers_for_draw, inpt):
+        """
+        This will compare numbers for deaw against drawn numbers for the round
+        inpt: 0 -> user defined numbers
+        inpt: 1 -> random defined numbers
+        """
+        guessed = len(set(numbers_for_draw).intersection(self.drawn_numbers))
 
-        if guessed == 0: self.guessed_zero += 1
-        if guessed == 1: self.guessed_one += 1
-        if guessed == 2: self.guessed_two += 1
-        if guessed == 3: self.guessed_three += 1
-        if guessed == 4: self.guessed_four += 1
-        if guessed == 5: self.guessed_five += 1
-        if guessed == 6: self.guessed_six += 1
-        self.toss_counter += 1
+        if guessed == 0: self.guessed_zero[inpt] += 1
+        if guessed == 1: self.guessed_one[inpt] += 1
+        if guessed == 2: self.guessed_two[inpt] += 1
+        if guessed == 3: self.guessed_three[inpt] += 1
+        if guessed == 4: self.guessed_four[inpt] += 1
+        if guessed == 5: self.guessed_five[inpt] += 1
+        if guessed == 6: self.guessed_six[inpt] += 1
 
     def print_results(self):
         """This prints current result after every 1 000 toss"""
         table = PrettyTable()
 
-        table.field_names = [texts['table_f1'][self.lang], texts['table_f2'][self.lang], texts['table_f3'][self.lang]]
+        table.field_names = [texts['table_f1'][self.lang],
+                             texts['table_gap1'][self.lang],
+                             texts['table_f2'][self.lang],
+                             texts['table_f3'][self.lang],
+                             texts['table_gap2'][self.lang],
+                             texts['table_f4'][self.lang],
+                             texts['table_f5'][self.lang]]
+
         table.align[texts['table_f1'][self.lang]] = "l"
         table.align[texts['table_f2'][self.lang]] = "r"
         table.align[texts['table_f3'][self.lang]] = "r"
-        table.add_row([f"0 {texts['num'][self.lang]}", self.guessed_zero, self.count_percentage(self.guessed_zero)])
-        table.add_row([f"1 {texts['nums'][self.lang]}", self.guessed_one, self.count_percentage(self.guessed_one)])
-        table.add_row([f"2 {texts['numss'][self.lang]}", self.guessed_two, self.count_percentage(self.guessed_two)])
-        table.add_row([f"3 {texts['numss'][self.lang]}", self.guessed_three, self.count_percentage(self.guessed_three)])
-        table.add_row([f"4 {texts['numss'][self.lang]}", self.guessed_four, self.count_percentage(self.guessed_four)])
-        table.add_row([f"5 {texts['numss'][self.lang]}", self.guessed_five, self.count_percentage(self.guessed_five)])
-        table.add_row([f"6 {texts['numss'][self.lang]}", self.guessed_six, self.count_percentage(self.guessed_six)])
+        table.align[texts['table_f4'][self.lang]] = "r"
+        table.align[texts['table_f5'][self.lang]] = "r"
+
+        table.add_row([f"0 {texts['num'][self.lang]}",
+                       '',               
+                       self.guessed_zero[0], self.count_percentage(self.guessed_zero[0]),
+                       '',
+                       self.guessed_zero[1], self.count_percentage(self.guessed_zero[1])]),
+        table.add_row([f"1 {texts['nums'][self.lang]}",
+                       '',               
+                       self.guessed_one[0], self.count_percentage(self.guessed_one[0]),
+                       '',
+                       self.guessed_one[1], self.count_percentage(self.guessed_one[1])]),
+        table.add_row([f"2 {texts['numss'][self.lang]}",
+                       '',               
+                       self.guessed_two[0], self.count_percentage(self.guessed_two[0]),
+                       '',
+                       self.guessed_two[1], self.count_percentage(self.guessed_two[1])]),
+        table.add_row([f"3 {texts['numss'][self.lang]}",
+                       '',               
+                       self.guessed_three[0], self.count_percentage(self.guessed_three[0]),
+                       '',
+                       self.guessed_three[1], self.count_percentage(self.guessed_three[1])]),
+        table.add_row([f"4 {texts['numss'][self.lang]}",
+                       '',               
+                       self.guessed_four[0], self.count_percentage(self.guessed_four[0]),
+                       '',
+                       self.guessed_four[1], self.count_percentage(self.guessed_four[1])]),
+        table.add_row([f"5 {texts['numss'][self.lang]}",
+                       '',               
+                       self.guessed_five[0], self.count_percentage(self.guessed_five[0]),
+                       '',
+                       self.guessed_five[1], self.count_percentage(self.guessed_five[1])]),
+        table.add_row([f"6 {texts['numss'][self.lang]}",
+                       '',               
+                       self.guessed_six[0], self.count_percentage(self.guessed_six[0]),
+                       '',
+                       self.guessed_six[1], self.count_percentage(self.guessed_six[1])]),
 
         system('cls')
         print(f"{texts['draw'][self.lang]}{self.toss_counter}")
-        print(f"{texts['your_nums'][self.lang]}{self.lottery_numbers}")
         print(f"{texts['drawn_nums'][self.lang]}{self.drawn_numbers}")
-        print(table, end='\r')
+        print(table)  # .get_string(title=f"{texts['title'][self.lang]}", end='\r'))
 
     def count_years(self):
         """This counts the time evaluated to win"""
@@ -140,8 +175,8 @@ class Lottery:
         """In case of 6 numbers of 6 was guessed correctly - means lottery is won, the tossing will stop"""
         print()
         print(f"!!! {texts['won1'][self.lang]}{self.toss_counter}!!!")
-        print(f"{texts['your_was'][self.lang]} {self.lottery_numbers} {texts['drawn_was'][self.lang]} "
-              f"{self.drawn_numbers}!!!")
+        # print(f"{texts['your_was'][self.lang]} {self.defined_numbers_for_draw} {texts['drawn_was'][self.lang]} "
+        # f"{self.drawn_numbers}!!!")
         print(f"{texts['won2'][self.lang]} {self.count_years()} {texts['won3'][self.lang]}")
 
 
--- src/texts.py
+++ src/texts.py
@@ -1,14 +1,16 @@
 # coding=utf-8
 
 texts = {
-    'intro1': ['Please input your 6 numbers from 1 to 49', 'Prosím zadaj 6 čísel od 1 do 49'],
-    'intro2': ['Example: 2 7 13 28 29 32 46', 'Príklad: 2 7 13 28 29 32 46'],
-    'intro3': ['If you want every draw random numbers, just press ENTER: ',
-               'Ak si želáš staviť v každom ťahu náhodné čísla, stlač ENTER: '],
+    'intro1': ['Please input your 6 numbers from 1 to 49 and press ENTER.', 'Prosím zadaj 6 čísel od 1 do 49 a stlač ENTER.'],
+    'intro2': ['(example: 2 7 13 28 29 32 46)', '(príklad: 2 7 13 28 29 32 46)'],
+    # 'intro3': ['If you want every draw random numbers, just press ENTER: ',
+    #            'Ak si želáš staviť v každom ťahu náhodné čísla, stlač ENTER: '],
     'val_error': ['Please enter numbers exactly the way specified.', 'Prosím zadaj čísla presne ako je špecifikované'],
     'table_f1': ["Guessed", "Uhádnutých"],
-    'table_f2': ["Times", "Počet krát"],
-    'table_f3': ["Success %", "Úspešnosť %"],
+    'table_f2': ["Times1", "Počet krát1"],
+    'table_f3': ["Success1 %", "Úspešnosť1 %"],
+    'table_f4': ["Times2", "Počet krát2"],
+    'table_f5': ["Success2 %", "Úspešnosť2 %"],
     'num': ["numbers", "čísel"],
     'nums': ["number", "číslo"],
     'numss': ["numbers", "čísel"],
@@ -20,5 +22,8 @@ texts = {
     'drawn_was': ['and drawn numbers was', 'a vylosované čísla boli'],
     'won2': ['If you will put a lottery 1 per week, you need approximatelly',
              'Ak budeš vsádzať 1 krát do týždňa, budeš potrebovať približne '],
-    'won3': ['years to win.', 'rokov na výhru.']
+    'won3': ['years to win.', 'rokov na výhru.'],
+    'table_gap1': ['x', 'x'],
+    'table_gap2': ['y', 'y'],
+    'title': ['      | your numbers  | random numbers', '     | tvoje čísla | náhodné čísla']
 }
