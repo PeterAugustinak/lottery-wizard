f12c69e76cb53c68919be823573812e956e40ea4
PeterAugustinakPersonal <peter.augustinak@gmail.com>
2021-09-15 17:56:54 +0200

    Code refinement based on pylint #1 + update of requirements.txt

--- src/lottery_cmd_output.py
+++ src/lottery_cmd_output.py
@@ -1,13 +1,13 @@
 # coding=utf-8
 """LotteryCmdOutput class"""
 
-# local library imports
-from texts import texts
-# external library imports
-from prettytable import PrettyTable
+# standard imports
 from itertools import islice
 from datetime import datetime, timedelta
-
+# external library imports
+from prettytable import PrettyTable
+# local library imports
+from texts import texts
 
 class LotteryCmdOutput:
     """Creates statistics tables for cmd output"""
@@ -21,7 +21,10 @@ class LotteryCmdOutput:
         self.table_chart = PrettyTable()
         self.current_date = None
 
-    def create_table_input(self, user_defined_numbers, random_numbers, draws_per_week):
+    def create_table_input(self, user_defined_numbers, random_numbers,
+                           draws_per_week):
+        """Method gathering inputs for table"""
+
         self.table_input.clear_rows()
         self.table_input.add_rows([
             [
@@ -42,6 +45,7 @@ class LotteryCmdOutput:
         return self.table_input
 
     def create_table_draw(self, draw_counter, drawn_numbers):
+        """Method builds draw table"""
         self.table_draw.clear_rows()
         self.table_draw.add_rows([
             [
@@ -64,8 +68,8 @@ class LotteryCmdOutput:
         """Counts passing the time by weeks"""
 
         if self.current_date:
-            next_date = self.current_date + timedelta(days=7) if draw_counter % self.draws_per_week == 0 \
-                else self.current_date
+            next_date = self.current_date + timedelta(days=7) if draw_counter\
+                % self.draws_per_week == 0 else self.current_date
         else:
             current_date = datetime.today()
             next_date = current_date
@@ -77,6 +81,7 @@ class LotteryCmdOutput:
 
     @staticmethod
     def current_week_year(date):
+        """Count week and year based on date"""
         week_year = datetime.date(date).isocalendar()
         week = week_year[1] if week_year[1] > 9 else f"0{week_year[1]}"
         year = week_year[0]
@@ -84,6 +89,7 @@ class LotteryCmdOutput:
         return week, year
 
     def create_table_stat(self, guessed_table, draw_counter):
+        """Runs building of table"""
         self.table_stat.title = texts['tab_title_stat'][self.lang]
 
         self.table_stat.field_names = [texts['table_f1'][self.lang],
@@ -105,25 +111,26 @@ class LotteryCmdOutput:
         self.table_stat.clear_rows()
         for i, key in zip(range(len(guessed_table)), guessed_table.keys()):
             if i == 0:
-                nm = 'numss'
+                nms = 'numss'
             elif i == 1:
-                nm = 'num'
+                nms = 'num'
             elif 1 < i < 5:
-                nm = 'nums'
+                nms = 'nums'
             else:
-                nm = 'numss'
+                nms = 'numss'
 
             guess_num = guessed_table[key]
 
             self.table_stat.add_row(
                 [
-                    f"{i} {texts[nm][self.lang]}",
+                    f"{i} {texts[nms][self.lang]}",
                     guess_num[0][0],
                     f"#{guess_num[0][1] if guess_num[0][1] > 0 else '-'}",
                     self.count_percentage(draw_counter, guess_num[0][0]),
                     guess_num[1][0],
                     f"#{guess_num[1][1] if guess_num[1][1] > 0 else '-'}",
-                    self.count_percentage(draw_counter, guessed_table[key][1][0]),
+                    self.count_percentage(draw_counter,
+                                          guessed_table[key][1][0]),
                 ]
              )
 
@@ -131,11 +138,16 @@ class LotteryCmdOutput:
 
     @staticmethod
     def count_percentage(draw_counter, correct_guesses):
-        """This counts percentage value of how many guess of the particular number of total draw was success"""
+        """This counts percentage value of how many guess of the particular
+         number of total draw was success"""
+
         return round(100 / draw_counter * correct_guesses, 3)
 
     def create_table_chart(self, amount_of_draw_numbers, numbers_chart):
-        self.table_chart.title = f"TOP {amount_of_draw_numbers}{texts['tab_title_chart'][self.lang]}"
+        """Creates table chart"""
+
+        self.table_chart.title = f"TOP {amount_of_draw_numbers}" \
+                                 f"{texts['tab_title_chart'][self.lang]}"
         self.table_chart.field_names = [texts['chart_top_nm'][self.lang],
                                         texts['chart_top_val'][self.lang],
                                         texts['chart_down_nm'][self.lang],
@@ -144,11 +156,14 @@ class LotteryCmdOutput:
         self.table_chart.align[texts['chart_top_val'][self.lang]] = "r"
         self.table_chart.align[texts['chart_down_val'][self.lang]] = "r"
 
-        sorted_chart_top = dict(sorted(numbers_chart.items(), key=lambda item: item[1], reverse=True))
-        sorted_chart_down = dict(sorted(numbers_chart.items(), key=lambda item: item[1]))
+        sorted_chart_top = dict(sorted(numbers_chart.items(), key=lambda item:
+                                item[1], reverse=True))
+        sorted_chart_down = dict(sorted(numbers_chart.items(), key=lambda item:
+                                        item[1]))
 
         top = list(islice(sorted_chart_top.items(), 0, amount_of_draw_numbers))
-        down = list(islice(sorted_chart_down.items(), 0, amount_of_draw_numbers))
+        down = list(islice(sorted_chart_down.items(), 0,
+                           amount_of_draw_numbers))
 
         self.table_chart.clear_rows()
         for n_top, n_down in zip(top, down[::-1]):
@@ -157,15 +172,17 @@ class LotteryCmdOutput:
         return self.table_chart
 
     def random_numbers_won(self, random_numbers_won, draw):
+        """In case of win"""
+
         week, year = self.current_week_year(self.current_date)
 
-        print(f"{texts['random_won1'][self.lang]} {' '.join(map(str, random_numbers_won))} "
+        print(f"{texts['random_won1'][self.lang]} "
+              f"{' '.join(map(str, random_numbers_won))} "
               f"{texts['random_won2'][self.lang]}{draw}!!!")
-        print(f"{texts['random_won3'][self.lang]} {week}, {texts['random_won4'][self.lang]} {year}.")
-
+        print(f"{texts['random_won3'][self.lang]} {week}, "
+              f"{texts['random_won4'][self.lang]} {year}.")
 
 #
 # cmd = LotteryCmdOutput(0, 2)
 # cmd.current_date = datetime.today()
 # cmd.random_numbers_won([1, 2, 3, 4, 5, 6], 3434)
-
